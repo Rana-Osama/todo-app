338f76d366ccc918f4201e298e7bb9f430510d6f
Rana-Osama <90419549+Rana-Osama@users.noreply.github.com>
2023-10-18 21:06:21 +0300

    Revert "+ firestore"

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/Providers/AuthProvider.dart';
-import 'package:todo_app/splash/splashScreen.dart';
 import 'package:todo_app/ui/home/HomeScreen.dart';
 import 'package:todo_app/ui/login/LoginScreen.dart';
 import 'package:todo_app/ui/register/RegisterScreen.dart';
@@ -9,10 +8,13 @@ import 'package:todo_app/ui/register/RegisterScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async {
+
+
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   runApp(ChangeNotifierProvider(
-      create: (BuildContext) => AuthProvider(), child: const MyApp()));
+      create: (BuildContext) => AuthProvider(),
+      child: const MyApp()));
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -30,18 +32,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         primaryColor: Colors.blue,
-        floatingActionButtonTheme:
-            FloatingActionButtonThemeData(backgroundColor: Colors.blue),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.blue
+        ),
         scaffoldBackgroundColor: Color(0xFFDFECDB),
         useMaterial3: false,
       ),
+
       routes: {
-        RegisterScreen.routeName: (_) => RegisterScreen(),
-        LoginScreen.routeName: (_) => LoginScreen(),
-        HomeScreen.routeName: (_) => HomeScreen(),
-        SplashScreen.routeName: (_) => SplashScreen()
+        RegisterScreen.routeName : (_)=> RegisterScreen(),
+        LoginScreen.routeName: (_)=> LoginScreen(),
+        HomeScreen.routeName: (_)=>HomeScreen(),
       },
-      initialRoute: SplashScreen.routeName,
+      initialRoute: LoginScreen.routeName,
     );
   }
 }
+
